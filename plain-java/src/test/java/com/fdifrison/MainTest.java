1d2c7b4c4bba36a0720c1071bd5c35e1e5403a90
giovanni frison <giovanni.frison@consultant.aruba.it>
2024-09-18 15:30:12 +0200

    Project init

---
+++ plain-java/src/test/java/com/fdifrison/MainTest.java
@@ -0,0 +1,13 @@
+package com.fdifrison;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void doNothing() {
+        assertTrue(true);
+    }
+}
