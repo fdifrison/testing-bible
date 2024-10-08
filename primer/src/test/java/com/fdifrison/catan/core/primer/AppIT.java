5ed000ac992a4a965626c3c6735bb938983f5ecc
fdifrison <ing.giovanni.frison@gmail.com>
2024-10-08 17:37:01 +0200

    surefire and failsafe

---
+++ primer/src/test/java/com/fdifrison/catan/core/primer/AppIT.java
@@ -0,0 +1,11 @@
+package com.fdifrison.catan.core.primer;
+
+import org.junit.jupiter.api.Test;
+
+public class AppIT {
+
+    @Test
+    void integrationTest() {
+        System.out.println("Executed by FailSafe");
+    }
+}
--- primer/src/test/java/com/fdifrison/catan/core/primer/AppTests.java
+++ primer/src/test/java/com/fdifrison/catan/core/primer/AppTests.java
@@ -1,13 +1,11 @@
 package com.fdifrison.catan.core.primer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PrimerApplicationTests {
+class AppTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Executed by SureFire");
     }
-
 }
