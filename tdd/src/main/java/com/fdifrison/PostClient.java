18748cbfafb2472f6a10e5933f6093b020b384ae
fdifrison <ing.giovanni.frison@gmail.com>
2025-05-07 15:27:54 +0200

    added tdd module

---
+++ tdd/src/main/java/com/fdifrison/PostClient.java
@@ -0,0 +1,8 @@
+package com.fdifrison;
+
+/**
+ * Develop an HTTP client that fetch all posts from the post-service and return them as a list. The
+ * post-service returns the all posts with pagination. The client should fetch all pages and return
+ * the result as a list.
+ */
+public class PostClient {}
---
+++ tdd/src/main/java/com/fdifrison/TimeUtil.java
@@ -0,0 +1,9 @@
+package com.fdifrison;
+
+/**
+ * Develop a feature to display information when a comment was made (one day ago, 3 days ago, 6
+ * month ago, etc.) in a human-readable format: - A comment that is older than 365 days, should
+ * return 'more than a year'. - A comment within today should return 'today'. - A date in the future
+ * is invalid and should throw an exception.
+ */
+public class TimeUtil {}
